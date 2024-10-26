53f99a5cef5a8477fcd98f6cc1e900baa95e46e0
Alexs Ruiz <ralexs.acu@gmail.com>
2024-10-26 00:39:28 -0600

    Use the CronService

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,11 @@
+import { CronService } from './cron/cron-service';
+
 export class Server {
-  public static start(){
-    console.log("Server running...");
+  public static start() {
+    console.log('Server running...');
+
+    CronService.createJob('*/5 * * * * *', () => {
+      console.log('5 Seconds');
+    });
   }
 }
