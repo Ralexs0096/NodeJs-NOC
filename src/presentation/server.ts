2f3c95133bd2b4ecb57dc0cecf503b9361db1582
Alexs Ruiz <ralexs.acu@gmail.com>
2024-10-28 00:01:12 -0600

    Provide the file system log repository to the check services

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,11 @@
 import { CheckService } from '../domain/use-cases/checks/check-service';
+import { FileSystemDataSource } from '../infrastructure/datasources/file-system.datasource';
+import { LogRepositoryImpl } from '../infrastructure/repositories/log.repository';
 import { CronService } from './cron/cron-service';
+
+const fileSystemLogRepository = new LogRepositoryImpl(
+  new FileSystemDataSource()
+);
 
 export class Server {
   public static start() {
@@ -7,8 +13,10 @@ export class Server {
 
     CronService.createJob('*/5 * * * * *', () => {
       const url = 'https://google.com';
+      // const url = 'https://localhost:3000';
 
       new CheckService(
+        fileSystemLogRepository,
         () => console.log(`${url} is OK`),
         (error) => console.log(error)
       ).execute(url);
