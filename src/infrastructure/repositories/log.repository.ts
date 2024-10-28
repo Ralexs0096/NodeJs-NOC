13b603c3d271820173de4c2d3f1ad647977680a4
Alexs Ruiz <ralexs.acu@gmail.com>
2024-10-27 23:46:04 -0600

    Implement the LogRepository methods

--- src/infrastructure/repositories/log.repository.ts
+++ src/infrastructure/repositories/log.repository.ts
@@ -1,11 +1,14 @@
+import { LogDataSource } from '../../domain/datasources/log.datasource';
 import { LogEntity, LogSeverityLevel } from '../../domain/entities/log.entity';
 import { LogRepository } from '../../domain/repository/log.repository';
 
 export class LogRepositoryImpl implements LogRepository {
-  saveLog(log: LogEntity): Promise<void> {
-    throw new Error('Method not implemented.');
+  constructor(private readonly logDatasource: LogDataSource) {}
+
+  async saveLog(log: LogEntity): Promise<void> {
+    this.logDatasource.saveLog(log);
   }
-  getLogs(severityLevel: LogSeverityLevel): Promise<Array<LogEntity>> {
-    throw new Error('Method not implemented.');
+  async getLogs(severityLevel: LogSeverityLevel): Promise<Array<LogEntity>> {
+    return this.logDatasource.getLogs(severityLevel);
   }
 }
