1de39cdd957f88a860c39d1fd42deab60e84ad3c
Alexs Ruiz <ralexs.acu@gmail.com>
2024-10-29 15:07:28 -0600

    Add sendEmail method

--- src/presentation/email/email.service.ts
+++ src/presentation/email/email.service.ts
@@ -1,6 +1,13 @@
 import nodemailer from 'nodemailer';
 import { envs } from '../../config/plugins/envs.plugin';
 
+interface SendMailOptions {
+  to: string;
+  subject: string;
+  htmlBody: string;
+  // TODO: Implement the attachment option
+}
+
 export class EmailService {
   private transporter = nodemailer.createTransport({
     service: envs.EMAIL_SERVICE,
@@ -9,4 +16,19 @@ export class EmailService {
       pass: envs.MAILER_SECRET_KEY
     }
   });
+
+  async sendEmail(options: SendMailOptions): Promise<boolean> {
+    const { htmlBody, subject, to } = options;
+    try {
+      await this.transporter.sendMail({
+        to,
+        subject,
+        html: htmlBody
+      });
+
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
 }
