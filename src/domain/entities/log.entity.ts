15e979f8042949d76fd7de6aa7c4f46516ee83dc
Alexs Ruiz <ralexs.acu@gmail.com>
2024-10-29 14:39:29 -0600

    Add a new attribute to the log Entity

--- src/domain/entities/log.entity.ts
+++ src/domain/entities/log.entity.ts
@@ -4,25 +4,39 @@ export enum LogSeverityLevel {
   high = 'high'
 }
 
+interface LogEntityOptions {
+  level: LogSeverityLevel;
+  message: string;
+  origin: string;
+  createdAt?: Date;
+}
+
 export class LogEntity {
   public level: LogSeverityLevel;
   public message: string;
   public createdAt: Date;
+  public origin: string;
 
-  constructor(message: string, level: LogSeverityLevel) {
+  constructor(options: LogEntityOptions) {
+    const { level, message, origin, createdAt = new Date() } = options;
     this.level = level;
     this.message = message;
+    this.origin = origin;
     this.createdAt = new Date();
   }
 
   static fromJson = (json: string): LogEntity => {
-    const { message, level, createdAt } = JSON.parse(json);
+    const { message, level, createdAt, origin } = JSON.parse(json);
     [message, level, createdAt].forEach((value) => {
       if (!value) throw new Error(`${value} is required`);
     });
 
-    const log = new LogEntity(message, level);
-    log.createdAt = new Date(createdAt);
+    const log = new LogEntity({
+      message,
+      level,
+      origin,
+      createdAt
+    });
 
     return log;
   };
