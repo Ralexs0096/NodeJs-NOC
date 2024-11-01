0fc813dcb8e2ac0ef4b5115970434c5ff34b88ed
Alexs Ruiz <ralexs.acu@gmail.com>
2024-11-01 11:24:18 -0600

    Use Mongo as datasource

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,22 +1,30 @@
+import { SendEmailLogs } from './../domain/use-cases/email/send-email-logs';
 import { CheckService } from '../domain/use-cases/checks/check-service';
 import { FileSystemDataSource } from '../infrastructure/datasources/file-system.datasource';
 import { LogRepositoryImpl } from '../infrastructure/repositories/log.repository';
 import { CronService } from './cron/cron-service';
+import { EmailService } from './email/email.service';
+import { MongoLogDatasource } from '../infrastructure/datasources/mongo-log.datasource';
 
-const fileSystemLogRepository = new LogRepositoryImpl(
-  new FileSystemDataSource()
+const logRepository = new LogRepositoryImpl(
+  // new FileSystemDataSource()
+  new MongoLogDatasource()
 );
+const emailService = new EmailService();
 
 export class Server {
   public static start() {
     console.log('Server running...');
 
+    // new SendEmailLogs(emailService, fileSystemLogRepository).execute(
+    //   'xiopito2@gmail.com'
+    // );
+
     CronService.createJob('*/5 * * * * *', () => {
       const url = 'https://google.com';
-      // const url = 'https://localhost:3000';
 
       new CheckService(
-        fileSystemLogRepository,
+        logRepository,
         () => console.log(`${url} is OK`),
         (error) => console.log(error)
       ).execute(url);
