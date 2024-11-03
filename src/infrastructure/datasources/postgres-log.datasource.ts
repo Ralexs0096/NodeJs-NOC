f2750d9d853429ed47c6d78722c520051436ba3b
Alexs Ruiz <ralexs.acu@gmail.com>
2024-11-02 23:54:37 -0600

    Add postgres log datasource implementation

---
+++ src/infrastructure/datasources/postgres-log.datasource.ts
@@ -0,0 +1,23 @@
+import { PrismaClient, SeverityLevel } from '@prisma/client';
+import { LogDataSource } from '../../domain/datasources/log.datasource';
+import { LogEntity, LogSeverityLevel } from '../../domain/entities/log.entity';
+
+const prismaClient = new PrismaClient();
+
+export class PostgresLogDatasource implements LogDataSource {
+  async saveLog(log: LogEntity): Promise<void> {
+    const level = log.level.toUpperCase() as SeverityLevel;
+    await prismaClient.logModel.create({
+      data: {
+        ...log,
+        level
+      }
+    });
+  }
+
+  async getLogs(severityLevel: LogSeverityLevel): Promise<Array<LogEntity>> {
+    const logs = await prismaClient.logModel.findMany();
+
+    return logs.map(LogEntity.fromObject);
+  }
+}
